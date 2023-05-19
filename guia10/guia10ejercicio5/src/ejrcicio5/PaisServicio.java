54a50fe489a993912e0abe4d9a6ae79d9a17a0a0
Agustin Giaccone <agustintgiaccone1999@gmail.com>
2023-05-19 17:23:44 -0300

    service 5 y 6

--- guia10/guia10ejercicio5/src/ejrcicio5/PaisServicio.java
+++ guia10/guia10ejercicio5/src/ejrcicio5/PaisServicio.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ejrcicio5;
+package ejercicio5;
 
-/**
- *
- * @author agust
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class PaisServicio {
-    
+
+    TreeSet<String> paises = new TreeSet();
+    Scanner leer = new Scanner(System.in).useDelimiter("\n");
+
+    public void crearPais() {
+
+        String respuesta = "si";
+        while (respuesta.equalsIgnoreCase("si")) {
+            System.out.println("Ingrese un pais");
+            String pais = leer.next();
+            paises.add(pais);
+            System.out.println("desea ingresar otro pais?[SI/NO]");
+            respuesta = leer.next();
+        }
+        System.out.println("todos los paises son:");
+        System.out.println(paises);
+        System.out.println("--------------------");
+
+    }
+
+    public void paisesOrdenAlfabeticamente() {
+
+        ArrayList<String> p = new ArrayList(paises);
+        Collections.sort(p);
+        System.out.println("los paises ordenados alfabeticamente");
+        System.out.println(p);
+        System.out.println("---------------------");
+        Iterator<String> iterator = p.iterator();
+        System.out.println("Ingrese que paise desea eliminar de la lista");
+        String eliminarPais = leer.next();
+        while (iterator.hasNext()) {
+            if (iterator.next().equalsIgnoreCase(eliminarPais)) {
+                iterator.remove();
+                System.out.println(p);
+                return;
+            }
+        }
+        System.out.println("no se encontro");
+    }
 }
--- guia10/guia10ejercicio6/src/ejercicio6/ProductoServicio.java
+++ guia10/guia10ejercicio6/src/ejercicio6/ProductoServicio.java
@@ -1,14 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio6;
 
-/**
- *
- * @author agust
- */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*Se necesita una aplicación para una tienda en la cual queremos almacenar los distintos
+productos que venderemos y el precio que tendrán. Además, se necesita que la
+aplicación cuente con las funciones básicas.
+Estas las realizaremos en el servicio. Como, introducir un elemento, modificar su precio,
+eliminar un producto y mostrar los productos que tenemos con su precio (Utilizar
+Hashmap). El HashMap tendrá de llave el nombre del producto y de valor el precio.
+Realizar un menú para lograr todas las acciones previamente mencionadas.*/
 public class ProductoServicio {
+    HashMap<String,Double>productos=new HashMap<>();
+    Scanner leer = new Scanner(System.in).useDelimiter("\n");
     
+    public void agregarProducto(){
+        System.out.println("Ingrese el nombre del producto: ");
+        String nombreProducto=leer.nextLine();
+        System.out.println("Ingrese el precio de ese producto: ");
+        double precio=leer.nextDouble();
+        leer.nextLine();
+        
+        if (productos.containsKey(nombreProducto)) {
+            System.out.println("el producto ya existe");
+        }
+        
+        productos.put(nombreProducto, precio);
+        System.out.println("!!!!!!producto agregado!!!!!!");
+    }
+    
+    public void modificarPrecio(){
+        System.out.println("Ingrese el nombre del producto: ");
+        String nombre= leer.nextLine();
+        if (productos.containsKey(nombre)) {
+            System.out.println("Ingrese el nuevo precio del producto: ");
+            double nuevoPrecio= leer.nextDouble();
+            
+            productos.put(nombre, nuevoPrecio);
+            System.out.println("el precio del producto se actualizo");
+        }else{
+            System.out.println("Ese producto no existe");
+        }
+    }
+    
+    public void eliminarProducto() {
+        System.out.print("Ingrese el nombre del producto a eliminar: ");
+        String nombre = leer.nextLine();
+        if (productos.containsKey(nombre)) {
+            productos.remove(nombre);
+            System.out.println("Producto eliminado correctamente.");
+        } else {
+            System.out.println("El producto no existe en la tienda.");
+        }
+    }
+    
+    public void mostrarProductos() {
+        System.out.println("------- PRODUCTOS -------");
+        for (Map.Entry<String, Double> entry : productos.entrySet()) {
+            String nombre = entry.getKey();
+            double precio = entry.getValue();
+            System.out.println(nombre + " - Precio: " + precio);
+        }
+    }
+            
 }
